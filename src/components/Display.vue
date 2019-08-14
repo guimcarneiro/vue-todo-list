3756e7debda8683b0735b47e5fb9d83a7ca88fd9
Guilherme Carneiro <guilherme.carneiro@ccc.ufcg.edu.br>
2019-08-14 15:45:19 -0300

    UserInput, Display components created, listing working

---
+++ src/components/Display.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="display">
+        <ul>
+            <li v-for="todo in todos" v-bind:key="todo.id">{{ todo.text }}</li>
+        </ul>
+        <UserInput v-on:userInputSubmit="handleSubmitTodo"></UserInput>
+    </div>
+</template>
+
+<script>
+  import UserInput from './UserInput.vue';
+
+export default {
+    name: 'TodosDisplay',
+    components: {
+        UserInput
+    },
+    data: function() {
+        return {
+            todos: []
+        }
+    },
+    methods: {
+        handleSubmitTodo: function(text) {
+            this.todos.push({
+                id: this.todos.length,
+                text
+            })
+        }
+    }
+}
+</script>
+
+<style>
+    #display li{
+        list-style-type: none;
+    }
+</style>
---
+++ src/components/UserInput.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="userInputContainer">
+        <input v-model="text" placeholder="Digite uma todo" />
+        <button v-on:click="submitted">Enviar</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'UserInput',
+    data: function() {
+        return {
+            text: ''
+        }
+    },
+    methods: {
+        submitted: function () {
+            this.$emit('userInputSubmit', this.text);
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
